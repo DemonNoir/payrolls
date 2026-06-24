01e06a43479c013a901fb49f3f04d05855e49cf6
Patawikon <43961400+Patawikon@users.noreply.github.com>
2026-06-24 21:54:50 +0700

    fix: use relative paths for PWA manifest and service worker to fix 404 on GitHub Pages

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='ot-v3';
-const FILES=['./','/index.html','/css/style.css','/js/utils.js','/js/calc.js','/js/ui.js','/js/settings.js','/js/data.js','/js/app.js','/manifest.json'];
+const FILES=['./','./index.html','./css/style.css','./js/utils.js','./js/calc.js','./js/ui.js','./js/settings.js','./js/data.js','./js/app.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
